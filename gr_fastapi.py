c59e8fa244d0476044ab5df261f1849d6129cb3f
yuqisun <7748145+yuqisun@users.noreply.github.com>
2023-08-09 23:14:06 +0800

    Create gr_fastapi.py

---
+++ gr_fastapi.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+import gradio as gr
+from starlette.staticfiles import StaticFiles
+
+CUSTOM_PATH = "/gradio"
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_main():
+    return {"message": "This is your main app"}
+
+
+io = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
+app = gr.mount_gradio_app(app, io, path=CUSTOM_PATH)
+
+
+# Run this from the terminal as you would normally start a FastAPI app: `uvicorn run:app`
+# and navigate to http://localhost:8000/gradio in your browser.
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
